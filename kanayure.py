7840680fb652ea420ebd7247ba23f5fefb6961f2
Masami HIRATA <msmhrt@gmail.com>
2012-12-20 20:52:14 +0900

    Fixed boundary argument from r"\"\s*\n\s*\"" to r"\"\s*\""

--- kanayure.py
+++ kanayure.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':  # pragma: nocover
 
     KanayureChecker(root_dir=r"..\py33",
                     include_files={"*.po"},
-                    boundary=r"\"\s*\n\s*\"")
+                    boundary=r"\"\s*\"")
