35977ea3559dbb7afebb26bdc84aca64629c9c62
Masami HIRATA <msmhrt@gmail.com>
2012-12-31 22:01:03 +0900

    Added KanayureCommand class

---
+++ kanayure/command.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# vim:fileencoding=utf-8
+
+
+class KanayureCommand:
+    def __init__(self):
+        pass
---
+++ tests/test_command.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# vim:fileencoding=utf-8
+
+import unittest
+
+
+class TestKanayureCommand(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_kanayurecommand(self):
+        pass
